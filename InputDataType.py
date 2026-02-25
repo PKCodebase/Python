2312ba5f101c58ef932e2c8c0abfaa6777c0487d
PKcodebase <kaushikprasad1659@gmail.com>
2026-02-25 22:40:59 +0530

    Ascii

---
+++ InputDataType.py
@@ -0,0 +1,11 @@
+# age =int(input("What is your age : "))
+# print(type(age))
+# print (age)
+
+
+name = str(input("What is your name : "))
+age =int(input("What is your age : "))
+print(type(age))
+print(type(name))
+
+print("name : ",name,"&","age : ",age)
---
+++ Operator.py
@@ -0,0 +1,19 @@
+a = 20
+b = 5
+
+print(a/b)
+print(a//b)  # ignore float values
+print(a%b)
+
+print(5**2)  # Power
+
+#compound assignment
+
+a = 20
+
+a +=  20
+
+a += 40
+
+a +=  60
+print(a)
