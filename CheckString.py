75e13695ef010685c3e181c63f3d5d301a861dac
PKcodebase <kaushikprasad1659@gmail.com>
2026-02-27 12:39:21 +0530

    Questions

---
+++ CheckString.py
@@ -0,0 +1,5 @@
+gender = str(input("Enter your gender :").lower())
+if gender == "male":
+    print("Good morning Sir")
+else :
+    print("Good morning madam")
---
+++ GreaterNo.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number :"))
+
+if a>b:
+    print("Greater Value",a)
+else:
+    print("Greater Value",b)
