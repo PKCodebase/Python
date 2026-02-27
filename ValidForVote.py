9690a1d4c6b2d5736ba7ac6ecbc6cc871fcb0d32
PKcodebase <kaushikprasad1659@gmail.com>
2026-02-27 14:16:13 +0530

    Question

---
+++ ValidForVote.py
@@ -0,0 +1,7 @@
+name = str(input("Enter the name : "))
+age = int(input("Enter the age : "))
+
+if age > 18:
+    print(name,"age : ",age," you are elligible for vote")
+else:
+    print(name,"age :",age,"you are not elligible for vote")
